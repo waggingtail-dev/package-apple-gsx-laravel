41ac64e25e73f19dfaccfbbe6fb8e272fa878827
naillizard <diego@waggingtail.co.nz>
2020-02-03 14:46:34 +1300

    Added getFacadeAceessor test

--- tests/Unit/FacadeTest.php
+++ tests/Unit/FacadeTest.php
@@ -15,4 +15,15 @@ class FacadeTest extends TestCase
 
         $this->assertTrue($reflection->isSubclassOf($facade));
     }
+
+    /** @test */
+    public function it_can_test_it_has_a_facade_accessor_method()
+    {
+        $reflection = new ReflectionClass('Waggingtail\AppleGsx\Laravel\Facades\AppleGsx');
+
+        $method = $reflection->getMethod('getFacadeAccessor');
+        $method->setAccessible(true);
+
+        $this->assertSame('applegsx', $method->invoke(null));
+    }
 }
